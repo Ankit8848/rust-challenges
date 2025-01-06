082c686dd5f06dafaefbe431d8d7b479e8a83911
ankit kumar jha <2100032609@kluniversity.in>
2025-01-07 01:14:29 +0530

    add problem find items

--- find_items/src/main.rs
+++ find_items/src/main.rs
@@ -1,15 +1,24 @@
-// Topic: working with a function
+// Topic: working with a Generics
 
 // Program requirements:
 // filters duplicates within a list(Vec(i32>)
 
 // Notes:
-// - Use a function to calculate the median.
-// - Handle three cases:
-//   1. Even number of elements
-//   2. Odd number of elements
-//   3. Empty list
-// - Sort the numbers before calculation.
+// - Use a Generics 
+// change unique() to accept a Vec<T>, where T is a type that implements Ord trait.
+
+// - Implement unique()
+// it accepts a Vec<i32> as an argument, 
+// returning a Vec<i32> with no dublicates.
+
+//   1. no dublicates in the input list.
+// let list = vec![1,2,3];
+// assert_eq!(unique(list), list);
+
+//   2. dublicates in the input list.
+//  let list = vec![1,2,2];
+// assert_eq(unique(list), unique_function)
+
 
 
 
