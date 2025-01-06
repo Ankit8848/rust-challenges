d837865c53029e0f437e17bb79ad81a2f01073f5
ankit kumar jha <2100032609@kluniversity.in>
2025-01-07 02:37:37 +0530

    add print text type

---
+++ print_text_type/src/main.rs
@@ -0,0 +1,16 @@
+// Topic: working with a function
+
+// Program requirements:
+// filters that can accept a String or a &str("string slice").
+
+// Notes:
+// - Implement info(text: &T)
+// it must accept (at least ) String and &str
+// print the input to stdout
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
