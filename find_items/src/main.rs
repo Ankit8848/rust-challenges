becd29151b14337ee3b6e15e51944f279da47cfb
ankit kumar jha <2100032609@kluniversity.in>
2025-01-07 01:11:17 +0530

    add problem find items

---
+++ find_items/src/main.rs
@@ -0,0 +1,18 @@
+// Topic: working with a function
+
+// Program requirements:
+// filters duplicates within a list(Vec(i32>)
+
+// Notes:
+// - Use a function to calculate the median.
+// - Handle three cases:
+//   1. Even number of elements
+//   2. Odd number of elements
+//   3. Empty list
+// - Sort the numbers before calculation.
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
